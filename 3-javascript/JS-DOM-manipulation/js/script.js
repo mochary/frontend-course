be2f4a411ced9c1745fc6637c4c84d0e86a02b51
Ran Mochary <ran.mochary@gong.io>
2021-01-18 21:11:46 +0200

    js dom manipulation exercise

--- 3-javascript/JS-DOM-manipulation/js/script.js
+++ 3-javascript/JS-DOM-manipulation/js/script.js
@@ -1,25 +1,56 @@
+
+function loadTweet(data, template, container) {
+    let clone = template.content.cloneNode(true);
+    clone.querySelector('.profile-image').setAttribute('src', data.profileImage);
+    clone.querySelector('.user-name').innerHTML = data.userName;
+    clone.querySelector('.feed-item-content').innerHTML = data.content;
+    clone.querySelector('.replies').innerHTML = data.replies;
+    clone.querySelector('.retweets').innerHTML = data.retweets;
+    clone.querySelector('.likes').innerHTML = data.likes;
+    container.appendChild(clone);
+}
+
 function loadTweets() {
-    let tweetContainer = document.getElementById('news-feed-container');
-    let template = document.getElementById('feed-item-template');
+    let feedItemTemplate = document.getElementById('feedItemTemplate');
+
+    let tweetContainer = document.getElementById('newsFeedContainer');
     for (let tweet of this.tweets) {
-        let clone = template.content.cloneNode(true);
-        clone.querySelector('.profile-image').setAttribute('src', tweet.profileImage);
-        clone.querySelector('.user-name').innerHTML = tweet.userName;
-        clone.querySelector('.feed-item-content').innerHTML = tweet.content;
-        clone.querySelector('.replies').innerHTML = tweet.replies;
-        clone.querySelector('.retweets').innerHTML = tweet.retweets;
-        clone.querySelector('.likes').innerHTML = tweet.likes;
-        tweetContainer.appendChild(clone);
+        loadTweet(tweet, feedItemTemplate, tweetContainer);
+    }
+
+    // load my top 3 tweets
+    tweetContainer = document.getElementById('myTopTweetsContainer');
+    for (let tweet of this.tweets.slice(0,3)) {
+        loadTweet(tweet, feedItemTemplate, tweetContainer);
     }
 }
 
 function loadUserData() {
+    let profileFeedDetailsTemplate = document.getElementById('profileFeedDetails');
+    let clone = profileFeedDetailsTemplate.content.cloneNode(true);
+    clone.querySelector('.profile-name').innerHTML = user.name;
+    clone.querySelector('.subtitle').innerHTML = this.tweets.length + ' Tweets';
+    document.getElementById('profileTop').appendChild(clone);
+    document.getElementById('profileFeed').querySelector('.cover-image').setAttribute('src', user.coverImagePath);
 
-}
+    let leftProfileDetails = document.getElementById('leftProfileDetails');
+    leftProfileDetails.querySelector('.profile-image').setAttribute('src', user.profileImagePath);
+    clone = profileFeedDetailsTemplate.content.cloneNode(true);
+    clone.querySelector('.profile-name').innerHTML = user.name;
+    clone.querySelector('.subtitle').innerHTML = '@' + user.name;
+    leftProfileDetails.appendChild(clone);
 
-window.onload = () => {
-    loadUserData();
-    loadTweets();
+    let profileFeed = document.getElementById('profileFeed');
+    profileFeed.querySelector('.about-me').innerHTML = user.aboutMe;
+    profileFeed.querySelector('.location .grayed-text').innerHTML = user.location;
+    profileFeed.querySelector('.website-link a').setAttribute('href', user.link);
+    profileFeed.querySelector('.website-link a').innerHTML = user.link;
+
+    profileFeed.querySelector('.joined-date .grayed-text').innerHTML = 'Joined ' + user.joined;
+
+    let followContainer = document.getElementById('followContainer');
+    followContainer.querySelector('.following .count').innerHTML = user.following;
+    followContainer.querySelector('.followers .count').innerHTML = user.followers;
 }
 
 function handleHomeMenuItemClick() {
@@ -29,6 +60,11 @@ function handleHomeMenuItemClick() {
 
 function handleProfileMenuItemClick() {
     document.getElementById("newsFeed").style.display = 'none';
-    document.getElementById("profileFeed").style.display = 'block';
+    document.getElementById("profileFeed").style.display = 'flex';
 
+}
+
+window.onload = () => {
+    loadUserData();
+    loadTweets();
 }
---
+++ 3-javascript/JS-DOM-manipulation/js/user.js
@@ -0,0 +1,12 @@
+this.user = {
+   name: 'sparkysparkyboomboom',
+   coverImagePath: 'assets/fuzzy-evening.jpg',
+   profileImagePath: 'assets/sparky.png',
+   aboutMe: 'Skillful in the art of various amounts of different ways to perform multiple actions in parallel on the beach',
+   location: 'Tel Aviv',
+   link: 'https://titanicbelfast.com/',
+   joined: 'March 2052',
+   following: 15,
+   followers: 3
+};
+
