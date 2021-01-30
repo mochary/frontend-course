9f3791b3da5cd917fa0ccee099cda83b71dad922
Ran Mochary <ran.mochary@gong.io>
2021-01-30 10:42:14 +0200

    js edit user profile form - initial version

---
+++ 3-javascript/JS-edit-user-profile-form/js/tweets.js
@@ -0,0 +1,27 @@
+this.tweets = [
+    {
+        profileImage : 'assets/sparky.png',
+        userName: 'Mahatma Gandhi',
+        content: 'First they ignore you, then they laugh at you, then they fight you, then you win.',
+        replies: 0,
+        retweets: '17.5M',
+        likes: '523M'
+    },
+    {
+        profileImage : 'assets/sparky.png',
+        userName: 'John Doe',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam amet aperiam aspernatur aut deserunt, earum id impedit laboriosam molestias nisi ratione recusandae repudiandae voluptate voluptatibus? Dolorum ullam vel veritatis.',
+        replies: 20,
+        retweets: 3,
+        likes: '1.1K'
+    },
+    {
+        profileImage : 'assets/sparky.png',
+        userName: 'Michael Jordan',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur deserunt facere magnam molestiae nam obcaecati qui quidem, unde voluptatibus? Aspernatur labore, nihil odio officia omnis perferendis quas suscipit voluptate.',
+        replies: 1,
+        retweets: 1,
+        likes: '2K'
+    }
+];
+
